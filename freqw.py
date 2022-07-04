4ac0887f8b318b8d1093a8252ff169c95d255f9d
Wesleyaz <wesleyaz1@hotmail.com>
2022-07-03 21:52:42 -0400

    int

---
+++ freqw.py
@@ -0,0 +1,17 @@
+#sent = input('Enter setence: ')
+'''
+file = input('Enter file: ')
+if not file:
+    file = "tfile.txt"
+hfile = open(file)
+
+for line in hfile:
+    words = line.split()
+    counts = { word:words.count(word) for word in words}
+
+
+
+#counts = { word:sent.count(word) for word in sent }
+print(counts)
+'''
+
