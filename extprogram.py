3c7b86865da60f0d1914c2be56bcdbc8c3366c4b
Wesleyaz <wesleyaz1@hotmail.com>
2022-07-16 15:59:13 -0400

    urllib

--- extprogram.py
+++ extprogram.py
@@ -1,6 +1,11 @@
 import socket
+import urllib.request, urllib.error, urllib.parse
 class Webhttp:
-    def httpreq():
+    def browserlib():
+        hurl = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+        for line in hurl:
+            print(line.decode().strip())
+    def httpreqconnect():
         mysock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         mysock.connect(('data.pr4e.org', 80))
         cmd = 'GET http:/data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
@@ -12,4 +17,5 @@ class Webhttp:
             print(data.decode())
         mysock.close()
 
-#Webhttp.httpreq()
+#Webhttp.httpconnect()
+Webhttp.browserlib()
