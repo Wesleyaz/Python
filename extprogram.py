5a7fb3252aee076cc4b00243bfadba609520f5d4
Wesleyaz <wesleyaz1@hotmail.com>
2022-07-16 21:46:08 -0400

    wordfreq from webpage

--- extprogram.py
+++ extprogram.py
@@ -1,14 +1,29 @@
 import socket
+from collections import Counter
+import urllib
 import urllib.request, urllib.error, urllib.parse
 class Webhttp:
     def browserlib():
         hurl = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
         for line in hurl:
             print(line.decode().strip())
+    def countwebtxt():
+        hurl = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+        wordfreq = dict()
+        for line in hurl:
+            words = line.decode().split()
+            for word in words:
+                wordfreq[word] = wordfreq.get(word, 0) + 1
+        lst = [ (v,k) for k,v in wordfreq.items() ]
+        lst = sorted(lst, reverse=True)
+        for v,k in lst[:10]:
+            print(k,v)
+        #print(wordfreq)
+
     def httpreqconnect():
         mysock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         mysock.connect(('data.pr4e.org', 80))
-        cmd = 'GET http:/data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+        cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
         mysock.send(cmd)
         while True:
             data = mysock.recv(512)
@@ -18,4 +33,5 @@ class Webhttp:
         mysock.close()
 
 #Webhttp.httpconnect()
-Webhttp.browserlib()
+#Webhttp.browserlib()
+Webhttp.countwebtxt()
